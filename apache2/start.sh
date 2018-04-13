8f861a6df62de55d5bd0e602ffde507c62c0781c
Bart Moorman <bmoorman@Hydro.id10t.us>
2018-04-13 09:32:24 -0600

    Unclean shutdown prevented apache from starting

--- apache2/start.sh
+++ apache2/start.sh
@@ -23,6 +23,16 @@ if [ ! -d /config/httpd/ssl ]; then
     ln --symbolic --force /etc/ssl/private/ssl-cert-snakeoil.key /config/httpd/ssl/organizr.key
 fi
 
+pidfile=/var/run/apache2/apache2.pid
+
+if [ -f ${pidfile} ]; then
+    pid=$(cat ${pidfile})
+
+    if [[ ! -d /proc/${pid} || ( -d /proc/${pid} && $(basename $(readlink /proc/${pid}/exe)) != 'apache2' ) ]]; then
+      rm ${pidfile}
+    fi
+fi
+
 exec $(which apache2ctl) \
     -D FOREGROUND \
     -D ${HTTPD_SECURITY:-HTTPD_SSL} \
